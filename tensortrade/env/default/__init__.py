87dcbe3b99a57a6d57e8347906c75f3b0b897fc2
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-04 00:08:54 +0430

    Update __init__.py

--- tensortrade/env/default/__init__.py
+++ tensortrade/env/default/__init__.py
@@ -1,4 +1,3 @@
-
 from typing import Union
 
 from . import actions
@@ -9,6 +8,7 @@ from . import informers
 from . import renderers
 
 from tensortrade.env.generic import TradingEnv
+from tensortrade.env.generic import TradingEnv_v1
 from tensortrade.env.generic.components.renderer import AggregateRenderer
 from tensortrade.feed.core import DataFeed
 from tensortrade.oms.wallets import Portfolio
@@ -20,10 +20,10 @@ def create(portfolio: 'Portfolio',
            feed: 'DataFeed',
            window_size: int = 1,
            min_periods: int = None,
-           **kwargs) -> TradingEnv:
+           train: bool = True,
+           **kwargs) -> TradingEnv or TradingEnv_v1:
     """Creates the default `TradingEnv` of the project to be used in training
     RL agents.
-
     Parameters
     ----------
     portfolio : `Portfolio`
@@ -41,49 +41,61 @@ def create(portfolio: 'Portfolio',
         The minimum number of steps to warm up the `feed`.
     **kwargs : keyword arguments
         Extra keyword arguments needed to build the environment.
-
     Returns
     -------
     `TradingEnv`
         The default trading environment.
     """
-
     action_scheme = actions.get(action_scheme) if isinstance(action_scheme, str) else action_scheme
     reward_scheme = rewards.get(reward_scheme) if isinstance(reward_scheme, str) else reward_scheme
-
     action_scheme.portfolio = portfolio
-
-    observer = observers.TensorTradeObserver(
-        portfolio=portfolio,
-        feed=feed,
-        renderer_feed=kwargs.get("renderer_feed", None),
-        window_size=window_size,
-        min_periods=min_periods
-    )
-
-    stopper = stoppers.MaxLossStopper(
-        max_allowed_loss=kwargs.get("max_allowed_loss", 0.5)
-    )
-
-    renderer_list = kwargs.get("renderer", renderers.EmptyRenderer())
-
-    if isinstance(renderer_list, list):
-        for i, r in enumerate(renderer_list):
-            if isinstance(r, str):
-                renderer_list[i] = renderers.get(r)
-        renderer = AggregateRenderer(renderer_list)
-    else:
-        if isinstance(renderer_list, str):
-            renderer = renderers.get(renderer_list)
+           
+    if train:
+        observer = observers.TensorTradeObserver(
+            portfolio=portfolio,
+            feed=feed,
+            renderer_feed=kwargs.get("renderer_feed", None),
+            window_size=window_size,
+            min_periods=min_periods,
+        )
+           
+        stopper = stoppers.MaxLossStopper(
+            max_allowed_loss=kwargs.get("max_allowed_loss", 0.5)
+        )
+           
+        renderer_list = kwargs.get("renderer", renderers.EmptyRenderer())
+           
+        if isinstance(renderer_list, list):
+            for i, r in enumerate(renderer_list):
+                if isinstance(r, str):
+                    renderer_list[i] = renderers.get(r)
+            renderer = AggregateRenderer(renderer_list)
         else:
-            renderer = renderer_list
+            if isinstance(renderer_list, str):
+                renderer = renderers.get(renderer_list)
+            else:
+                renderer = renderer_list
+           
+        env = TradingEnv(
+            action_scheme=action_scheme,
+            reward_scheme=reward_scheme,
+            observer=observer,
+            stopper=kwargs.get("stopper", stopper),
+            informer=kwargs.get("informer", informers.TensorTradeInformer()),
+            renderer=renderer,
+        )
+           
+    else:
+        stopper = stoppers.MaxLossStopper(
+            max_allowed_loss=kwargs.get("max_allowed_loss", 0.5)
+        )
 
-    env = TradingEnv(
-        action_scheme=action_scheme,
-        reward_scheme=reward_scheme,
-        observer=observer,
-        stopper=kwargs.get("stopper", stopper),
-        informer=kwargs.get("informer", informers.TensorTradeInformer()),
-        renderer=renderer
-    )
+        env = TradingEnv_v1(
+            action_scheme=action_scheme,
+            reward_scheme=reward_scheme,
+            stopper=kwargs.get("stopper", stopper),
+            informer=kwargs.get("informer", informers.TensorTradeInformer()),
+            window_size=window_size,
+        )
+
     return env
