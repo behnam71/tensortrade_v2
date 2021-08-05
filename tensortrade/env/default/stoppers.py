c097b90a5ae54ce4e12f501068dcd58206090214
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-05 19:24:44 +0430

    Update stoppers.py

--- tensortrade/env/default/stoppers.py
+++ tensortrade/env/default/stoppers.py
@@ -1,5 +1,5 @@
 
-from tensortrade.env.generic import Stopper, TradingEnv
+from tensortrade.env.generic import Stopper, TradingEnv, TradingEnv_v1
 
 
 class MaxLossStopper(Stopper):
@@ -31,3 +31,31 @@ class MaxLossStopper(Stopper):
         c1 = env.action_scheme.portfolio.profit_loss > self.max_allowed_loss
         c2 = not env.observer.has_next()
         return c1 or c2
+    
+class MaxLossStopper_v1(Stopper):
+    """A stopper that stops an episode if the portfolio has lost a particular
+    percentage of its wealth.
+
+    Parameters
+    ----------
+    max_allowed_loss : float
+        The maximum percentage of initial funds that is willing to
+        be lost before stopping the episode.
+
+    Attributes
+    ----------
+    max_allowed_loss : float
+        The maximum percentage of initial funds that is willing to
+        be lost before stopping the episode.
+
+    Notes
+    -----
+    This stopper also stops if it has reached the end of the observation feed.
+    """
+
+    def __init__(self, max_allowed_loss: float):
+        super().__init__()
+        self.max_allowed_loss = max_allowed_loss
+
+    def stop(self, env: 'TradingEnv') -> bool:
+        return env.action_scheme.portfolio.profit_loss > self.max_allowed_loss
