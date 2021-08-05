2a31649700bf6f493b460031e701606996b43a0e
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-05 20:29:07 +0430

    Update environment.py

--- tensortrade/env/generic/environment.py
+++ tensortrade/env/generic/environment.py
@@ -11,30 +11,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License
-
 import uuid
 import logging
-
-from typing import Dict, Any, Tuple
+from typing import Union, Tuple, List, Dict, Any
 
 import gym
 import numpy as np
+import ccxt
 
+from tensortrade.oms.services.execution.ccxt import CCXTExchange
 from tensortrade.core import TimeIndexed, Clock, Component
 from tensortrade.env.generic import (
     ActionScheme,
     RewardScheme,
-    Observer,
     Stopper,
-    Informer,
-    Renderer
+    Informer
 )
+from tensortrade.features import FeaturePipeline
+from tensortrade.features.indicators.talib_indicator import TAlibIndicator
 
 
-class TradingEnv(gym.Env, TimeIndexed):
+class TradingEnv_v1(gym.Env, TimeIndexed):
     """A trading environment made for use with Gym-compatible reinforcement
     learning algorithms.
-
     Parameters
     ----------
     action_scheme : `ActionScheme`
@@ -53,7 +52,6 @@ class TradingEnv(gym.Env, TimeIndexed):
     kwargs : keyword arguments
         Additional keyword arguments needed to create the environment.
     """
-
     agent_id: str = None
     episode_id: str = None
 
@@ -64,6 +62,7 @@ class TradingEnv(gym.Env, TimeIndexed):
                  stopper: Stopper,
                  informer: Informer,
                  renderer: Renderer,
+                 train: bool,
                  **kwargs) -> None:
         super().__init__()
         self.clock = Clock()
@@ -81,11 +80,31 @@ class TradingEnv(gym.Env, TimeIndexed):
         self.action_space = action_scheme.action_space
         self.observation_space = observer.observation_space
 
+        credentials = { 
+            'apiKey': 'SmweB9bNM2qpYkgl4zaQSFPpSzYpyoJ6B3BE9rCm0XYcAdIE0b7n6bm11e8jMwnI',  
+            'secret': '8x6LtJztmIeGPZyiJOC7lVfg2ixCUYkhVV7CKVWq2LVlPh8mo3Ab7SMkaC8qTZLt',
+        }
+        self.ccxt = CCXTExchange(
+            exchange='binance',
+            credentials=credentials,
+        )
+        
+        self._window_size = window_size
+        self._train = train
+        
+        with open("/mnt/c/Users/BEHNAMH721AS.RN/OneDrive/Desktop/indicators.txt", "r") as file:
+            indicators_list = eval(file.readline())
+        TAlib_Indicator = TAlibIndicator(indicators_list)
+        self.feature_pipeline = FeaturePipeline(
+            steps=[TAlib_Indicator]
+        )
+
         self._enable_logger = kwargs.get('enable_logger', False)
         if self._enable_logger:
             self.logger = logging.getLogger(kwargs.get('logger_name', __name__))
             self.logger.setLevel(kwargs.get('log_level', logging.DEBUG))
 
+            
     @property
     def components(self) -> 'Dict[str, Component]':
         """The components of the environment. (`Dict[str,Component]`, read-only)"""
@@ -98,14 +117,43 @@ class TradingEnv(gym.Env, TimeIndexed):
             "renderer": self.renderer
         }
 
+    
+    @property
+    def feature_pipeline(self) -> FeaturePipeline:
+        """The pipeline of feature transformations to pass the observations through at each time step."""
+        return self._feature_pipeline
+
+    @feature_pipeline.setter
+    def feature_pipeline(self, feature_pipeline: Union[FeaturePipeline, str] = None):
+        self._feature_pipeline = features.get(feature_pipeline) if isinstance(
+            feature_pipeline, str
+        ) else feature_pipeline
+
+    def _next_observation(self) -> np.ndarray:
+        observation = self.ccxt.next_observation(self._window_size)
+        if self._feature_pipeline is not None:
+            observation = self._feature_pipeline.transform(observation)
+        
+        if len(observations) < self._window_size:
+            size = self.window_size - len(observations)
+            padding = np.zeros(size, len(self.observation_columns()))
+            padding = pd.DataFrame(padding, columns=self.observation_columns())
+            observations = pd.concat([padding, observation], ignore_index=True, sort=False)
+                
+        observations = self.observations.select_dtypes(include='number')
+        if isinstance(observations, pd.DataFrame):
+            observations = observations.fillna(0, axis=1)
+        observations = np.nan_to_num(self.observations)
+        
+        return observations.to_numpy()
+        
+        
     def step(self, action: Any) -> 'Tuple[np.array, float, bool, dict]':
         """Makes on step through the environment.
-
         Parameters
         ----------
         action : Any
             An action to perform on the environment.
-
         Returns
         -------
         `np.array`
@@ -120,18 +168,21 @@ class TradingEnv(gym.Env, TimeIndexed):
         """
         self.action_scheme.perform(self, action)
 
-        obs = self.observer.observe(self)
+        if self._train == True:
+            obs = self.observer.observe(self)
+        else:
+            obs = self._next_observation()
+            
         reward = self.reward_scheme.reward(self)
         done = self.stopper.stop(self)
         info = self.informer.info(self)
 
         self.clock.increment()
-
         return obs, reward, done, info
 
+    
     def reset(self) -> 'np.array':
         """Resets the environment.
-
         Returns
         -------
         obs : `np.array`
@@ -144,12 +195,15 @@ class TradingEnv(gym.Env, TimeIndexed):
             if hasattr(c, "reset"):
                 c.reset()
 
-        obs = self.observer.observe(self)
+        if self._train == True:
+            obs = self.observer.observe(self)
+        else:
+            obs = self._next_observation()
 
         self.clock.increment()
 
         return obs
-
+    
     def render(self, **kwargs) -> None:
         """Renders the environment."""
         self.renderer.render(self, **kwargs)
