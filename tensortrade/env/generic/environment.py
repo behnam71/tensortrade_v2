33b4e63429d4b8400a08dc306ee8231849dcc5af
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-10 14:43:46 +0430

    Update environment.py

--- tensortrade/env/generic/environment.py
+++ tensortrade/env/generic/environment.py
@@ -178,6 +178,8 @@ class TradingEnv(gym.Env, TimeIndexed):
             self.clock.increment()
         else:
             obs = self._next_observation()
+            info = self.informer.info(self, self._t_signal)
+            self.clock.increment()
             print("Online Observation:"); print(obs)
             return obs
         
