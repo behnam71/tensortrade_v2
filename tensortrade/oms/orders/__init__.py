0568516bbf1ad147c72a7854849fa78ffafdd825
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-10 23:06:47 +0430

    Update __init__.py

--- tensortrade/oms/orders/__init__.py
+++ tensortrade/oms/orders/__init__.py
@@ -1,10 +1,9 @@
 from tensortrade.oms.orders.trade import Trade, TradeSide, TradeType
+from tensortrade.oms.orders.order_v1 import Order_v1, OrderStatus
 from tensortrade.oms.orders.broker import Broker
 from tensortrade.oms.orders.order import Order, OrderStatus
 from tensortrade.oms.orders.order_listener import OrderListener
 from tensortrade.oms.orders.order_spec import OrderSpec
-
-from tensortrade.oms.orders.order_v1 import Order_v1, OrderStatus
 
 from tensortrade.oms.orders.create import (
     market_order,
