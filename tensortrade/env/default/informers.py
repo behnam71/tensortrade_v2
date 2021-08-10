553f483ef0113ce54df8b0ba26dceaae0af01583
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-10 14:56:11 +0430

    Update informers.py

--- tensortrade/env/default/informers.py
+++ tensortrade/env/default/informers.py
@@ -7,7 +7,7 @@ class TensorTradeInformer(Informer):
     def __init__(self) -> None:
         super().__init__()
 
-    def info(self, env: 'TradingEnv_v1') -> dict:
+    def info(self, env: 'TradingEnv') -> dict:
         return {
             'step': self.clock.step,
             'net_worth': env.action_scheme.portfolio.net_worth
