9530cdc5a34fdd9784eeb8c593e80d4b15a885e8
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-11 13:53:46 +0430

    Update criteria.py

--- tensortrade/oms/orders/criteria.py
+++ tensortrade/oms/orders/criteria.py
@@ -195,11 +195,17 @@ class Stop(Criteria):
             percent = abs(price - order.price) / order.price
             is_take_profit = (self.direction == StopDirection.UP) and (price >= order.price)
             is_stop_loss = (self.direction == StopDirection.DOWN) and (price <= order.price)
+            print("55555555555555555555555555555555555555555555555555555555552222222")
+            print(percent)
+            print(self.percent)
+            print(is_take_profit)
+            print(is_stop_loss)
+            print((is_take_profit or is_stop_loss) and percent >= self.percent)
         else:
             percent = abs(price - order.price_online) / order.price_online
             is_take_profit = (self.direction == StopDirection.UP) and (price >= order.price_online)
             is_stop_loss = (self.direction == StopDirection.DOWN) and (price <= order.price_online)
-            print("5555555555555555555555555555555555555555555555555555555555")
+            print("55555555555555555555555555555555555555555555555555555555557777777")
             print(percent)
             print(self.percent)
             print(is_take_profit)
