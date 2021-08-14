31753e14d5fa9b84d7a3029cd41c9262d6a74068
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-14 20:50:35 +0430

    Update ccxt.py

--- tensortrade/oms/services/execution/ccxt.py
+++ tensortrade/oms/services/execution/ccxt.py
@@ -63,7 +63,7 @@ class CCXTExchange():
                 
 
     def UTC_Time(self):
-        now_utc = datetime.now(timezone.utc) + timedelta(hours=4)
+        now_utc = datetime.now(timezone.utc)
         now_utc = datetime.strftime(now_utc, "%Y-%m-%d %H:00:00")
         return datetime.strptime(now_utc, "%Y-%m-%d %H:00:00")
 
