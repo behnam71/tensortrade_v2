db2f115aa69e19fea88958bd30e8a4c84b0ca385
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-27 18:14:11 +0430

    Update simulated.py

--- tensortrade/oms/services/execution/simulated.py
+++ tensortrade/oms/services/execution/simulated.py
@@ -186,11 +186,10 @@ def execute_order(order: 'Order',
     """
     if not(t_signal):
         print("Executing Order:")
-        print(float(order.price_online))
-        print(float(order.quantity))
-        print(float(order.quantity)/float(order.price_online))
-                    
         pprint(order)
+        print('price: {}'.format(str(order.price_online)))
+        print('quantity: {}'.format(str(order.quantity)))
+        print(float(order.quantity)/float(order.price_online))
     
     kwargs = {"order": order,
               "base_wallet": base_wallet,
