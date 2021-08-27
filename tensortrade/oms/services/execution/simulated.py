1c8b2860bdfe64958440f2711a4665695956ae9e
behnam heydari <behnamheydari.7.21@gmail.com>
2021-08-27 17:58:19 +0430

    Update simulated.py

--- tensortrade/oms/services/execution/simulated.py
+++ tensortrade/oms/services/execution/simulated.py
@@ -186,10 +186,9 @@ def execute_order(order: 'Order',
     """
     if not(t_signal):
         print("Executing Order:")
-        print(type(order))
-        print(order['price_online'])
-        print(order['quantity'])
-        print(order['quantity']/order['price_online'])
+        print(order.price_online)
+        print(order.quantity)
+        print(order.quantity/order.price_online)
                     
         pprint(order)
     
