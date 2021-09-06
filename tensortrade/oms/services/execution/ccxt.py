76ba60be894d8cc4717137021af696b8a0cae223
behnam heydari <behnamheydari.7.21@gmail.com>
2021-09-06 12:16:32 +0430

    Update ccxt.py

--- tensortrade/oms/services/execution/ccxt.py
+++ tensortrade/oms/services/execution/ccxt.py
@@ -71,7 +71,7 @@ class CCXTExchange():
         return datetime.strptime(now_utc, "%Y-%m-%d %H:%M:00")
 
     def next_observation(self, window_size: int) -> pd.DataFrame:
-        self._prev_ft = self._prev_ft + timedelta(minutes=2*3)
+        self._prev_ft = self._prev_ft + timedelta(minutes=3)
         self._prev_ft = datetime.strftime(self._prev_ft, "%Y-%m-%d %H:%M:00")
         self._prev_ft = datetime.strptime(self._prev_ft, "%Y-%m-%d %H:%M:00")
         print("2222222222222222222222222222222222222222222222222222")
@@ -101,6 +101,8 @@ class CCXTExchange():
         )
         self._Obs_DB.drop_duplicates(subset=['date'], keep='first', inplace=True)
         self._Obs_DB = self._Obs_DB.reset_index(drop=True)
+        print("44444444444444444444444444444444444444444444444444444444")
+        print(self._Obs_DB)
 
         return self._Obs_DB
 
